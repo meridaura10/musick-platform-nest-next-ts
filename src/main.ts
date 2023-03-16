d113ffbb53e3c4c7ce7cbdcf550c0bcc2097eeee
meridaura10 <112898856+meridaura10@users.noreply.github.com>
2023-03-16 12:19:36 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ const start = async () => {
   try {
     const PORT = process.env.PORT || 4444;
     const app = await NestFactory.create(AppModule,{cors: false});
+    app.enableCors({
+      origin: '*',
+      methods: ['GET', 'PUT', 'POST', 'DELETE', 'PATCH', 'OPTIONS'],
+      allowedHeaders: ['Content-Type', 'Authorization'],
+    });
     await app.listen(PORT, () => {
       console.log(`server started on PORT ${PORT}`);
     });
