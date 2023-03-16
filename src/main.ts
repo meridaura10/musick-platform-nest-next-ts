9db478a394dc84126458d4ef81a928c488d22e63
meridaura10 <meridaura10@gmail.com>
2023-03-16 12:44:34 +0200

    f

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,18 @@ import { AppModule } from './app.module';
 const start = async () => {
   try {
     const PORT = process.env.PORT || 4444;
-    const app = await NestFactory.create(AppModule,{cors: false});
+    const app = await NestFactory.create(AppModule);
+    app.use((req, res, next) => {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+      res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+      next();
+    });
+  
+    app.enableCors({
+      allowedHeaders: '*',
+      origin: '*',
+    });
     await app.listen(PORT, () => {
       console.log(`server started on PORT ${PORT}`);
     });
