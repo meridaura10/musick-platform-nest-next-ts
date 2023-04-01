22ae3b0692e4f9397f04cfc352d0d069fcc089b1
meridaura10 <meridaura10@gmail.com>
2023-04-01 21:13:26 +0300

    f

--- src/track/schemas/track.schema.ts
+++ src/track/schemas/track.schema.ts
@@ -1,34 +1,24 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 import * as mongoose from 'mongoose';
-
 export type TrackDocument = Track & Document;
-
 @Schema()
 export class Track {
   @Prop()
   name: string;
-
   @Prop()
   artist: string;
-
   @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User' })
   creator: mongoose.Schema.Types.ObjectId;
-
   @Prop() 
   text: string;
-
   @Prop()
   listens: number;
-
   @Prop({ type: mongoose.Schema.Types.Mixed })
   picture: Record<string, string>;
-
   @Prop({ type: mongoose.Schema.Types.Mixed })
   audio: Record<string, string>;
-
   @Prop({ type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }] })
   comments: Comment[];
 }
-
 export const TrackSchema = SchemaFactory.createForClass(Track);
