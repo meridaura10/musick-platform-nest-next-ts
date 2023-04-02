97ce75f21d8ede71f31213b767aed5da61a143f7
meridaura10 <meridaura10@gmail.com>
2023-04-02 15:15:41 +0300

    f

--- next.config.js
+++ next.config.js
@@ -2,8 +2,7 @@
 const nextConfig = {
   reactStrictMode: false,
   env: {
-    BACKEND_URL: 'https://musick-platform-nest-next-ts-api-git-server-meridaura10.vercel.app/',
-    
+    BACKEND_URL: 'musick-platform-nest-next-ts-api.vercel.app/', 
   }
 }
 
